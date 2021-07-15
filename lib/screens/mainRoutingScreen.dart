747ab673cdabc369d24467a8ab3ee79db5d04147
ahadu Sefefe <ahadusefefe.123@gmail.com>
2021-07-15 11:55:33 +0300

    created basic screens

---
+++ lib/screens/mainRoutingScreen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:kirb/constants/cutomThemeData.dart';
+import 'package:kirb/screens/homeScreen.dart';
+import 'package:kirb/screens/profileScreen.dart';
+import 'package:kirb/screens/searchScreen.dart';
+
+class MainRoutingScreen extends StatefulWidget {
+  const MainRoutingScreen({Key? key}) : super(key: key);
+
+  @override
+  _MainRoutingScreenState createState() => _MainRoutingScreenState();
+}
+
+class _MainRoutingScreenState extends State<MainRoutingScreen> {
+  int _selectedIndex = 0;
+  List<Widget> _children = [HomeScreen(), SearchScreen(), ProfileScreen()];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: _children[_selectedIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 40,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home_outlined,
+              ),
+              label: "home"),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+              ),
+              label: "search"),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person_outline,
+            ),
+            label: "me",
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        unselectedItemColor: Colors.grey[500],
+        selectedItemColor: customThemeData.accentColor,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
