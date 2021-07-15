6ce30dcc3934bd3b825a6432a7c676ca61bd9eef
ahadu Sefefe <ahadusefefe.123@gmail.com>
2021-07-15 11:55:02 +0300

    created custom theme

---
+++ lib/constants/cutomThemeData.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+ThemeData customThemeData = ThemeData(
+    primaryColor: Colors.white,
+    accentColor: Colors.red,
+    iconTheme: IconThemeData(size: 20, color: Colors.grey),
+    primaryIconTheme: IconThemeData(size: 30, color: Colors.red));
